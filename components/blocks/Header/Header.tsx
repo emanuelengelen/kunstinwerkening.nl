5a4b7a4d4a739686894bd9728f8a494701233ad0
Emanuel Engelen <emanuelengelen@gmail.com>
2021-03-01 22:21:04 +0100

    refactor: Header

---
+++ components/blocks/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as Styles from './Header.styles';
+
+export default function Header() {
+    return (
+        <Styles.Header>
+            <h1>kunst in werking 2.0</h1>
+            <nav> test </nav>
+        </Styles.Header>
+    );
+}
